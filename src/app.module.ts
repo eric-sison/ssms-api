371bbb414bdabff574a445e24846344d961e0bbc
Eric Sison <ericsison.dev@gmail.com>
2024-01-15 01:55:19 +0800

    modified application configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupportType } from './api/support-types/data/support-types.entity';
+import { SupportTypesModule } from './api/support-types/core/support-types.module';
+import { Category } from './api/categories/data/categories.entity';
+import { CategoriesModule } from './api/categories/core/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // application configuration modules
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DB_HOST'),
+        port: parseInt(configService.getOrThrow('DB_PORT')),
+        username: configService.getOrThrow('DB_USER'),
+        password: configService.getOrThrow('DB_PASS'),
+        database: configService.getOrThrow('DB_NAME'),
+        entities: [SupportType, Category],
+        logging: true,
+        synchronize: true,
+      }),
+    }),
+
+    // application api route modules
+    SupportTypesModule,
+    CategoriesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
