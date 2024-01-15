b7f0b700944117064d7fdfd875701581800518df
Eric Sison <ericsison.dev@gmail.com>
2024-01-15 17:36:18 +0800

    added unique property to name column for support-type

--- src/api/support-types/data/support-types.entity.ts
+++ src/api/support-types/data/support-types.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity } from 'typeorm';
 
 @Entity('support_types')
 export class SupportType extends BaseEntity {
-  @Column({ type: 'varchar', length: 50, nullable: false })
+  @Column({ type: 'varchar', length: 50, nullable: false, unique: true })
   name: string;
 
   @Column({ type: 'varchar', length: 200, nullable: false })
