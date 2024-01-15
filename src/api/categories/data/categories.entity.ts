a9d184a3f59644542dc7a23bdc033d119791f6b1
Eric Sison <ericsison.dev@gmail.com>
2024-01-15 17:38:44 +0800

    added unique property in category name column

--- src/api/categories/data/categories.entity.ts
+++ src/api/categories/data/categories.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity } from 'typeorm';
 
 @Entity('categories')
 export class Category extends BaseEntity {
-  @Column({ type: 'varchar', length: 50, nullable: false })
+  @Column({ type: 'varchar', length: 50, nullable: false, unique: true })
   name: string;
 
   @Column({ type: 'varchar', length: 200, nullable: false })
