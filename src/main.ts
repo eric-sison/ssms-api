f43ec96c46ebc97d83d6b3f2e756099a703de08d
Eric Sison <ericsison.dev@gmail.com>
2024-01-19 16:12:16 +0800

    enabled credentials in cors and added origin fields. also added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://172.20.10.55:3000'],
+  });
+
+  app.use(helmet());
 
   app.enableVersioning({ type: VersioningType.URI });
 
